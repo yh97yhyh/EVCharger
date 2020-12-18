40355f71a3e6f177ffadde8e4f7eb58f5ebe35ec
yh97yhyh <yh97yhyh@naver.com>
2020-12-18 13:13:59 +0900

    첫화면 구현 완료

---
+++ MyEVStation_android/app/src/main/java/com/example/myevstation_android/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.myevstation_android;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.time.Duration;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivity";
+    private NaverMap mNaverMap;
+    private FusedLocationSource mLocationSource;
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS = {
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 지도 객체 생성성
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        // getMapAsync를 호출하여 비동기로 OnMapReady 콜백메서드 호출
+        // onMapReady에서 NaverMap 객체를 받음
+        mapFragment.getMapAsync(this);
+
+        // 위치를 반환하는 구체적인 FusedLocationSource 생성
+        mLocationSource = new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+
+        // Bottom Navigation
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile_item:
+                        Toast.makeText(getApplicationContext(), "프로필", Toast.LENGTH_SHORT).show();
+                    case R.id.filter_item:
+                        Toast.makeText(getApplicationContext(), "필터", Toast.LENGTH_SHORT).show();
+                    case R.id.favorite_item:
+                        Toast.makeText(getApplicationContext(), "즐겨찾기", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        Log.d(TAG, "onMapReady");
+
+        //지도 상에 마커 표시
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37, 126));
+        marker.setMap(naverMap);
+
+        //NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        mNaverMap = naverMap;
+//        mNaverMap.setMapType(NaverMap.MapType.Navi);
+        mNaverMap.getUiSettings().setLocationButtonEnabled(true);
+        mNaverMap.setLocationSource(mLocationSource);
+        mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow); // 위치에 따라 지도 카메라 이동
+
+        //권확확인. 결과는 onRequestPermissionResult 콜백 메서드 호출
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // request code와 권환획득 여부 확인
+        if(requestCode == PERMISSION_REQUEST_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
+    }
+}
