259015a9148af73674928cbae7b287cb9fe3a5e0
root <root@vmsingleubuntu1804.ggpdugb53xpurilmrtw1quuawc.syx.internal.cloudapp.net>
2021-01-02 08:55:03 +0000

    settings.py

--- MyEVStation_django/rest/rest/settings.py
+++ MyEVStation_django/rest/rest/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +119,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
